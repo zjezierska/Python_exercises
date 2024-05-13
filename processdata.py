b44df67c26ca975543539f9880630ae386c486c8
Zuzanna Jezierska <79923468+zjezierska@users.noreply.github.com>
2024-05-13 11:00:25 +0200

    Update processdata.py

--- processdata.py
+++ processdata.py
@@ -1,1 +1,17 @@
+def process_data(s):
+  new_list = []
+  
+  for dict in s:
+    new_dict = {}
+    new_dict["name"] = dict["name"]
+    new_dict["average_score"] = Average(dict["scores"])
+    new_dict["is_adult"] = dict["age"] >= 18
 
+    new_list.append(new_dict)
+
+return new_list
+  
+
+def Average(lst): 
+    return sum(lst) / len(lst) 
+  
